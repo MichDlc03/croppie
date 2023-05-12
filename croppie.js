edc19fe2a6af8c8b81e29a39bdb4102668624fcf
MichDlc03 <jimenemishell3@gmail.com>
2023-05-12 12:34:53 -0600

    Ya es funcional croppie3.html

--- croppie.js
+++ croppie.js
@@ -1,46 +1,44 @@
-$(document).ready(function() {
-    var $uploadCrop = $('#upload-demo').croppie({
-      enableExif: true,
-      viewport: {
-        width: 200,
-        height: 200,
-        type: 'square'
-      },
-      boundary: {
-        width: 300,
-        height: 300
-      }
-    });
-
-    $('#inputImagen').on('change', function(e) {
-      var file = e.target.files[0];
-      var reader = new FileReader();
-
-      reader.onload = function(e) {
-        $uploadCrop.croppie('bind', {
-          url: e.target.result
-        });
-        $('#myModal').modal('show');
-      }
-
-      reader.readAsDataURL(file);
-    });
-
-    $('#cropImage').on('click', function(e) {
-      $uploadCrop.croppie('result', {
-        type: 'canvas',
-        size: 'viewport'
-      }).then(function(response) {
-        $.ajax({
-          url: "guardar-img.php",
-          type: "POST",
-          data: {"image": response},
-          success: function(data) {
-            $('#uploaded_image').html(data);
-          }
-        });
-        // Cerrar el modal
-        $('#myModal').modal('hide');
+$(document).ready(function(){
+      $image_crop = $('#image_demo').croppie({
+        enableExif: true,
+        viewport: {
+          width: 200,
+          height: 200,
+          type: 'square' 
+        },
+        boundary:{
+          width: 300,
+          height: 300
+        }
       });
+
+      $('#upload_image').on('change', function(){
+        var reader = new FileReader();
+        reader.onload = function (event) {
+          $image_crop.croppie('bind', {
+            url: event.target.result
+          }) 
+        }
+        reader.readAsDataURL(this.files[0]);
+        $('#uploadimage').show();
+        $('#myModal').modal('show')
+      });
+
+      $('.crop_image').click(function(event){
+        $image_crop.croppie('result', {
+          type: 'canvas',
+          size: 'viewport'
+        }).then(function(response){
+          $.ajax({
+            url: "guardar-img.php",
+            type: "POST",
+            data: {"image": response},
+            success: function(data){
+              $('#uploaded_image').html(data)
+            }
+          });
+        })
+      });
+
+      
     });
-  });
---
+++ croppie3.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+var $uploadCrop = $('#upload-demo').croppie({
+    enableExif: true,
+    viewport: {
+    width: 200,
+    height: 200,
+    type: 'square'
+    },
+    boundary: {
+    width: 300,
+    height: 300
+    }
+});
+
+$('#inputImagen').on('change', function(e) {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+    $uploadCrop.croppie('bind', {
+        url: e.target.result
+    });
+    $('#myModal').modal('show');
+    }
+
+    reader.readAsDataURL(file);
+});
+
+$('#cropImage').on('click', function(e) {
+    $uploadCrop.croppie('result', {
+    type: 'canvas',
+    size: 'viewport'
+    }).then(function(response) {
+    $.ajax({
+        url: "guardar-img.php",
+        type: "POST",
+        data: {"image": response},
+        success: function(data) {
+        $('#uploaded_image').html(data);
+        
+        }
+    });
+    $('#myModal').modal('hide');
+    });
+});
+  });
